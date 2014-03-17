24090429a00631ffecbe888675be2045aa089ab3
Ilias Bartolini <ibartoli@thoughtworks.com>
2014-03-17 10:12:50 -0300

    Removing misleading informations

--- docpad.js
+++ docpad.js
@@ -14,10 +14,10 @@ module.exports = {
       state: "Pernambuco, Brazil"
     },
 
-    callToAction: {
-        text: "Register now!",
-        link: "http://eventick.com.br"
-    },
+    // callToAction: {
+    //     text: "Register now!",
+    //     link: "http://eventick.com.br"
+    // },
 
     site: {
       theme: "yellow-swan",
@@ -57,18 +57,16 @@ module.exports = {
         description: "Programa em fase de definicão"
       },
       {
-        name: "Linus Torvalds",
+        name: "Programacão Insubordinada",
         photo: "themes/yellow-swan/img/speaker.jpg",
         bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Linux Foundation",
         link: {
           href: "http://twitter.com/linus",
           text: "@linus"
         },
         presentation: {
-          title: "Digging into a Linux Kernel",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "10h00"
+          title: "Você quier ajudar na creacão desse evento?",
+          description: "",
         }
       }
     ],
