1d4b241c7d13b163a992b4603b0a494b4f754253
Ilias Bartolini <ibartoli@thoughtworks.com>
2014-03-23 21:29:10 -0300

    e59e484 Various content fixes

---
+++ themes/yellow-swan/js/main.js
@@ -0,0 +1,13 @@
+// 21 Dez 2012, 4:42
+;(function ($, window, document, undefined) {
+    'use strict';
+    ({
+        init : function () {
+            var that = this;
+
+            $(function () {
+
+            });
+        }
+    }).init();
+}(jQuery, window, document));
